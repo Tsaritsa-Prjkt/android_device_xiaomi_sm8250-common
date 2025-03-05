89b915b00685d1be387f0473a0e2a6e20216b83a
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    sm8250-common: Include Dolby Atmos
    * Using dolby from: https://github.com/swiitch-OFF-Lab/hardware_dolby
    * Sony and Motorola Dolby Atoms both support this adaption
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Change-Id: I99240aed6b3ad6ac16506f755512bf13c3f939bd
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -183,6 +183,9 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,qtidisplay,gralloc_handle_has_reserved_size,true)
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
