sm8250-stuffs/device_xiaomi_sm8250-common-new

sm8250-common: parts: Enable use_resource_processor for all sysui deps
This will make the build faster

parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -20,7 +20,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ThermalActivity extends CollapsingToolbarBaseActivity {
 
@@ -30,7 +29,7 @@ public class ThermalActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
     }
 

Tsaritsa-Prjkt/android_device_xiaomi_sm8250-common

Change-Id: I4b02c0b799e894f2350bab595354931b046ea303

parts/src/org/lineageos/settings/thermal/ThermalActivity.java
@@ -29,8 +29,7 @@ public class ThermalActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction().replace(
-                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+        getSupportFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new ThermalSettingsFragment(), TAG_THERMAL).commit();
     }
 