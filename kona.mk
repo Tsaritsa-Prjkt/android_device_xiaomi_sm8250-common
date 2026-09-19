15168142/device_xiaomi_sm8250-common

sm8250-common: Set camera override format from reserved via soong config
Change-Id: I6a089e783f26c4515940d74f8e912dffa2789550

kona.mk
@@ -144,6 +144,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore

Tsaritsa-Prjkt/android_device_xiaomi_sm8250-common

kona.mk
@@ -144,6 +144,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore